c5f76675dca77eafec0463650a6d2ea6efdc39fd
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-07-14 10:26:24 +0000

    update 1A.m
    added intensity value

--- Practical 1/1A.m
+++ Practical 1/1A.m
@@ -12,3 +12,12 @@ i=imread('cameraman.jpeg');
 figure;
 imshow(i);
 title('Grayscale Image');
+
+%disp(i);
+ad=im2double(i);
+%disp(ad);
+
+row=61;
+col=132;
+intensity=i(row,col);
+fprintf("Intensity at %d row and %d col is %d",row,col,intensity);
