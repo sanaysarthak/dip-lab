cff3937cb88a1dc367306105dd589cf60aec5bac
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-07-14 10:37:07 +0000

    refactored 1A.m

--- Practical 1/1A/1A.m
+++ Practical 1/1A/1A.m
@@ -1,4 +1,5 @@
-%{ 
+%{
+Practical 1-A
 Write a MATLAB program to display and access pixel intensity values in grayscale image.
 Use the file 'cameraman.jpeg' 
 %}
@@ -7,17 +8,17 @@ clc;
 clear;
 close all;
 
-i=imread('cameraman.jpeg');
+i = imread('cameraman.jpeg');
 
 figure;
 imshow(i);
 title('Grayscale Image');
 
 %disp(i);
-ad=im2double(i);
+ad = im2double(i);
 %disp(ad);
 
-row=61;
-col=132;
-intensity=i(row,col);
-fprintf("Intensity at %d row and %d col is %d",row,col,intensity);
+row = 61;
+col = 132;
+intensity = i(row,col);
+fprintf("Intensity at %d row and %d col is %d",row, col, intensity);
