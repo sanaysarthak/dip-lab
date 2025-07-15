412678aba2c957150e1bff11fadcafde5eb76af3
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-07-15 17:40:33 +0000

    update 3C.m

--- Practical 3/3C/3C.m
+++ Practical 3/3C/3C.m
@@ -1,29 +1,37 @@
 %{
 Practical 3-C
-Write a MATLAB program to peform uniform quantization.
-Use the file 'cameraman.jpeg' 
+Write a MATLAB program to perform uniform quantization.
+Use the file 'cam.jpg' 
 %}
 
 clc;
 clear;
 close all;
 
-i = imread('cameraman.jpeg');
+i=imread("cameraman.jpeg");
 ad = im2double(i);
-nbits = 2;
+
+nbits = 4; % see the change in quantized histogram after tweaking the value of nbits
 levels = 2^nbits;
 steps = 1/levels;
 
-bins = floor(ad/steps);
-qv = (bins*steps) + (steps/2);
+bins=floor(ad/steps);
+qv = (bins*steps)+(steps/2);
 qvimg = im2uint8(qv);
 
-subplot(1,2,1);
-imshow(qvimg);
-title('Quantized Image');
 
-subplot(1,2,2);
-histogram(qvimg(:), 'BinEdges', 0:256);
-title('Histogram of Quantized Image');
-xlabel('Pixel Value');
-ylabel('Frequency');
+subplot(2,2,1);
+imshow(i);
+title("Original Image");
+
+subplot(2,2,2);
+(imhist(i));
+title("Original Histogram")
+
+subplot(2,2,3);
+imshow(qvimg);
+title("Quantized Image");
+
+subplot(2,2,4);
+(imhist(qvimg));
+title("Quantized Histogram")
