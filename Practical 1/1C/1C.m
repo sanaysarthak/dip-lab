a15b907670c7a70a93f96a72a6118c52a797fd94
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-07-17 12:43:36 +0530

    Create 1C.m

---
+++ Practical 1/1C/1C.m
@@ -0,0 +1,22 @@
+%{
+Practical 1-C
+Write a MATLAB program to convert RGB image to Grayscale image.
+Use the file 'badminton.jpg' 
+%}
+
+clc;
+clear;
+close all;
+
+i = imread('badminton.jpg');
+j = rgb2gray(i);
+
+figure;
+
+subplot(1,2,1);
+imshow(i);
+title('Original Image');
+
+subplot(1,2,2);
+imshow(j);
+title('Grayscale Image');
