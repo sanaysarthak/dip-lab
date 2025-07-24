5e175f812d5e56f8881fb75be1fd95f591cd8458
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-07-24 05:15:53 +0000

    add four_neighbour.m

---
+++ Datewise Practical/2025-07-24/four_neighbour.m
@@ -0,0 +1,29 @@
+% Write a program in MATLAB to find 4-neighbour of any row, column value given from the user.
+
+clc;
+clear;
+close all;
+
+i = imread('cameraman.jpeg');
+i = rgb2gray(i);
+
+[row, column, ch] = size(i);
+
+fprintf("Image has the following Dimensions:-\n");
+fprintf("Rows: %d\n", row);
+fprintf("Columns: %d\n", column);
+
+fprintf("\nEnter Pixel values to get its 4-neighbour:-\n");
+r = input("Enter row: ");
+c = input("Enter column: ");
+
+if r < 1 || r > row || c < 1 || c > column
+    fprintf("Error: The entered pixel (%d, %d) is out of image bounds.\n", r, c);
+    return;  % terminate the script
+end
+
+fprintf("\nThe 4-neighbours are as follows:-\n");
+fprintf("Top:    (%d, %d)\n", r-1, c);
+fprintf("Bottom: (%d, %d)\n", r+1, c);
+fprintf("Left:   (%d, %d)\n", r, c-1);
+fprintf("Right:  (%d, %d)\n", r, c+1);
