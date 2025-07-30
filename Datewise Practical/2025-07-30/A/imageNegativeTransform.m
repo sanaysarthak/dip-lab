bab73424b8d89e59e79674c75795ca8aab7fac6f
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-07-30 06:35:28 +0000

    add image negative transform: code, image, output

---
+++ Datewise Practical/2025-07-30/A/imageNegativeTransform.m
@@ -0,0 +1,17 @@
+%{
+Practical
+Write a MATLAB program to perform image negative transformation.
+Use the file 'cameraman.jpeg' 
+%}
+
+img = imread('cameraman.jpeg');
+
+if size(img, 3) == 3
+    img = rgb2gray(img);
+end
+
+img_negative = 255 - img;
+
+figure;
+subplot(2, 2, 1); imshow(img); title('Original Image');
+subplot(2, 2, 2); imshow(img_negative); title('Negative Image');
