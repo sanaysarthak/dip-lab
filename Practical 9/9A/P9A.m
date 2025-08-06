b01c09f58afec0b69c99825e1ff1cec8cb803236
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-08-06 06:17:12 +0000

    add code for P9A.m

---
+++ Practical 9/9A/P9A.m
@@ -0,0 +1,25 @@
+%{
+Practical 9-A
+Write a MATLAB program to perfrom bit-level slicing of an image.
+Use the in-built command 'bitget()' to get individual bits.
+Showcase the output in a 2x4 grid.
+Use the files 'cameraman.jpeg'
+%}
+
+clc;
+clear;
+close all;
+
+i = imread('cameraman.jpeg');
+
+if size(i, 3) == 3
+    i = rgb2gray(i);
+end
+
+figure;
+for bit = 1:8
+    bit_plane = bitget(i, bit);
+    subplot(2, 4, bit);
+    imshow(logical(bit_plane)); 
+    title(['Bit Plane ', num2str(bit)]);
+end
