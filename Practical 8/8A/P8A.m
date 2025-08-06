40965e31833c935a3397a2fd534a94ee8a91af21
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-08-06 05:53:03 +0000

    add matlab code for P8A.m

---
+++ Practical 8/8A/P8A.m
@@ -0,0 +1,27 @@
+%{
+Practical 8-A
+Write a MATLAB program to perfrom contrast stretching.
+Don't use any built-in functions. Use the mathematical equations or formula accordingly.
+Use the files 'low.tif', 'pout.tif', and 'spine.tif'
+%}
+
+clc;
+clear;
+close all;
+
+i = imread('spine.tif');
+i = double(i);
+
+r1 = min(i(:));
+r2 = max(i(:));
+
+s1 = 0;    
+s2 = 255; 
+
+% Apply the contrast stretching formula:
+out = s1 + ((i - r1) / (r2 - r1)) * (s2 - s1);
+out = uint8(out);
+
+figure;
+subplot(1, 2, 1); imshow(uint8(i)); title('Original Image');
+subplot(1, 2, 2); imshow(out); title('Contrast Stretched Image');
