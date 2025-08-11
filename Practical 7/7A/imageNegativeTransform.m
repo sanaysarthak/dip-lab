3e17fe8be737ef1d82d9469b9bd2af71ac7923c8
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-08-11 11:16:55 +0530

    P7A.m: Add clc, clear, close all for clean start

--- Practical 7/7A/imageNegativeTransform.m
+++ Practical 7/7A/imageNegativeTransform.m
@@ -4,6 +4,10 @@ Write a MATLAB program to perform image negative transformation.
 Use the file 'cameraman.jpeg' 
 %}
 
+clc;
+clear;
+close all;
+
 img = imread('cameraman.jpeg');
 
 if size(img, 3) == 3
